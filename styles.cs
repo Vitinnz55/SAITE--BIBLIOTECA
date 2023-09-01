6b8330ad03554d9b5319fe64ea950f9b1f284f1f
Vitor Eduardo <110904565+Vitinnz55@users.noreply.github.com>
2023-09-01 08:27:51 -0300

    Add files via upload

---
+++ styles.cs
@@ -0,0 +1,17 @@
+:root{
+    --cor-de-fundo: #EBECEE;
+    --branco: #FFF;
+    --amarelo: #fecd2f;
+    --roxo: #C75EEB;      
+    --fonte-roboto: 'Roboto', sans-serif;
+    --fonte-principal: 'Poppins', sans-serif;
+    --fonte-secundaria:  'Josefin Sans', sans-serif;
+    --fonte-secundaria-normal: 400;
+    --fonte-secundaria-bold: 700;
+  }
+   
+body{
+    background-color: var(--cor-de-fundo);    
+    font-size: 16px;
+    font-weight: 400;
+}
